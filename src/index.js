8d1621a458b72d746ebeef81cc72a184803c9df1
TeemourA <taxprogramming@gmail.com>
2020-05-06 17:14:56 +0300

    fix tabs

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import program from 'commander';
 import makeJsonDiff from './jsonDiff.js';
 
 export default () => {
-	program
-		.description('Compares two configuration files and shows a difference.')
-		.version('0.0.1', '-v, --version', 'output the version number')
-		.helpOption('-h, --help', 'output usage information')
-		.option('-f, --format [type]', 'output format')
-		.arguments('<filepath1> <filepath2>')
-		.action((filepath1, filepath2) => {
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1', '-v, --version', 'output the version number')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
       const diff = makeJsonDiff(filepath1, filepath2);
-
       console.log(diff);
     });
 
-	program.parse(process.argv);
+  program.parse(process.argv);
 };
