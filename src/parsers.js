311fc8b475236c028804664f1eef1def5a34c051
TeemourA <taxprogramming@gmail.com>
2020-05-08 18:57:14 +0300

    add ini-format parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,18 +1,19 @@
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
-const supportedExtensions = ['.json', '.yml'];
+const supportedExtensions = ['.json', '.yml', '.ini'];
 
 export default (filepath1, filepath2) => {
   const ext = path.extname(filepath1);
   const ext1 = path.extname(filepath2);
 
   if (!supportedExtensions.includes(ext) || !supportedExtensions.includes(ext1)) {
-    throw new Error('Error: unsupported format');
+    throw new Error('Unsupported format');
   }
 
   if (ext !== ext1) {
-    throw new Error('Error: file types must be equal');
+    throw new Error('File types must be equal');
   }
 
   let parser;
@@ -21,6 +22,8 @@ export default (filepath1, filepath2) => {
     parser = JSON.parse;
   } else if (ext === '.yml') {
     parser = yaml.safeLoad;
+  } else if (ext === '.ini') {
+    parser = ini.parse;
   }
 
   return parser;
