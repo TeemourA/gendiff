271c87483af1f894a2f995fbc05ae16cd42d8c64
TeemourA <taxprogramming@gmail.com>
2020-05-13 00:00:32 +0300

    add [format] parameter to program

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
+import program from 'commander';
 import genDiff from '../index.js';
 
-genDiff();
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information');
+
+program
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2, program.format);
+    console.log(diff);
+  });
+
+program.parse(process.argv);
