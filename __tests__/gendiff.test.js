56f7ec9feaaf7886e4a39bbf7c358303c5e81911
TeemourA <taxprogramming@gmail.com>
2020-05-13 00:01:00 +0300

    update test for deep structures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,28 +1,77 @@
 import { test, expect } from '@jest/globals';
 import yaml from 'js-yaml';
 import ini from 'ini';
-import makeDiff from '../src/diff.js';
+import genDiff from '../src/index.js';
 import getParser from '../src/parsers.js';
 
 console.log(getParser);
 
-const expected = `{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
+const expected = {
+  plain: `{
   - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
   + verbose: true
-}`;
+}`,
+  deep: `{
+    common: {
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: {
+            key: value
+        }
+        setting6: {
+            key: value
+          + ops: vops
+        }
+      + follow: false
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
+}`,
+};
+
+const fixtures = {
+  before: {
+    json: './__fixtures__/deepBefore.json',
+    yaml: './__fixtures__/deepBefore.yml',
+    ini: './__fixtures__/deepBefore.ini',
+  },
+  after: {
+    json: './__fixtures__/deepAfter.json',
+    yaml: './__fixtures__/deepAfter.yml',
+    ini: './__fixtures__/deepAfter.ini',
+  },
+};
 
 test('parsers', () => {
-  expect(getParser('__fixtures__/before.json', '__fixtures__/after.json')).toBe(JSON.parse);
-  expect(getParser('__fixtures__/before.yml', '__fixtures__/after.yml')).toBe(yaml.safeLoad);
-  expect(getParser('__fixtures__/before.ini', '__fixtures__/after.ini')).toBe(ini.parse);
+  expect(getParser(fixtures.before.json, fixtures.after.json)).toBe(JSON.parse);
+  expect(getParser(fixtures.before.yaml, fixtures.after.yaml)).toBe(yaml.safeLoad);
+  expect(getParser(fixtures.before.ini, fixtures.after.ini)).toBe(ini.parse);
 });
 
-test('gendiff', () => {
-  expect(makeDiff('__fixtures__/before.json', '__fixtures__/after.json')).toBe(expected);
-  expect(makeDiff('__fixtures__/before.yml', '__fixtures__/after.yml')).toBe(expected);
-  expect(makeDiff('__fixtures__/before.ini', '__fixtures__/after.ini')).toBe(expected);
+test('gendiff [stylish]', () => {
+  expect(genDiff(fixtures.before.json, fixtures.after.json, 'stylish')).toBe(expected.deep);
+  expect(genDiff(fixtures.before.yaml, fixtures.after.yaml, 'stylish')).toBe(expected.deep);
+  expect(genDiff(fixtures.before.ini, fixtures.after.ini, 'stylish')).toBe(expected.deep);
 });
