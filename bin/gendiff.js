f6f9aa81abd56879181a8e7ab04c8e4756995679
TeemourA <taxprogramming@gmail.com>
2020-05-20 00:13:41 +0300

    chain methods call

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,7 @@ import genDiff from '../src/index.js';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-v, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
-
-program
+  .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
