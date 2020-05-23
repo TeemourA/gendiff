a6f70da2fa94fd1098ede13a1a38979c691c6091
TeemourA <taxprogramming@gmail.com>
2020-05-23 13:40:45 +0300

    add ext getter & minor fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import fs from 'fs';
+import path from 'path';
 import buildDiff from './diff.js';
-import getParser from './parsers.js';
-import getFormatter from './formatters/formatters.js';
+import parse from './parsers.js';
+import convertToFormat from './formatters/index.js';
+
+const getFileExt = (filepath) => {
+  const ext = path.extname(filepath).slice(1);
+
+  return ext === 'yml' ? 'yaml' : ext;
+};
 
 export default (filepath1, filepath2, format) => {
-  const toFormat = getFormatter(format);
-  const parse = getParser(filepath1, filepath2);
+  const data1 = fs.readFileSync(filepath1, 'utf-8');
+  const data2 = fs.readFileSync(filepath2, 'utf-8');
 
-  const objectFile1 = parse(fs.readFileSync(filepath1, 'utf-8'));
-  const objectFile2 = parse(fs.readFileSync(filepath2, 'utf-8'));
+  const objectFile1 = parse(data1, getFileExt(filepath1));
+  const objectFile2 = parse(data2, getFileExt(filepath2));
 
   const diff = buildDiff(objectFile1, objectFile2);
+  const converted = convertToFormat(diff, format);
 
-  return toFormat(diff);
+  return converted;
 };
