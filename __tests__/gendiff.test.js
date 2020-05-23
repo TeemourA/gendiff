96d34a2bac81e800c5638a77140ac9ad7c259648
TeemourA <taxprogramming@gmail.com>
2020-05-23 13:29:48 +0300

    remove parser test & add path getter

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,34 +1,16 @@
-import { test, expect } from '@jest/globals';
-import yaml from 'js-yaml';
-import ini from 'ini';
 import fs from 'fs';
+import path from 'path';
 import genDiff from '../src/index.js';
-import getParser from '../src/parsers.js';
 
 
-const paths = {
-  before: {
-    '.json': './__fixtures__/before.json',
-    '.yml': './__fixtures__/before.yml',
-    '.ini': './__fixtures__/before.ini',
-  },
-  after: {
-    '.json': './__fixtures__/after.json',
-    '.yml': './__fixtures__/after.yml',
-    '.ini': './__fixtures__/after.ini',
-  },
-};
-
-const parsers = {
-  '.json': JSON.parse,
-  '.yml': yaml.safeLoad,
-  '.ini': ini.parse,
-};
+const getPath = (filename) => (
+  path.join('.', '__fixtures__', filename)
+);
 
 const output = {
-  stylish: fs.readFileSync('./__fixtures__/stylish.txt', 'utf-8'),
-  plain: fs.readFileSync('./__fixtures__/plain.txt', 'utf-8'),
-  json: fs.readFileSync('./__fixtures__/json.txt', 'utf-8'),
+  stylish: fs.readFileSync('./__fixtures__/resultStylishOutput.txt', 'utf-8'),
+  plain: fs.readFileSync('./__fixtures__/resultPlainOutput.txt', 'utf-8'),
+  json: fs.readFileSync('./__fixtures__/resultJsonOutput.txt', 'utf-8'),
 };
 
 describe.each`
@@ -37,12 +19,8 @@ describe.each`
   ${'.yml'}
   ${'.ini'}
 `('gendiff for $ext files', ({ ext }) => {
-  const before = paths.before[ext];
-  const after = paths.after[ext];
-
-  test('parser', () => {
-    expect(getParser(before, after)).toBe(parsers[ext]);
-  });
+  const before = getPath(`before${ext}`);
+  const after = getPath(`after${ext}`);
 
   test('formatter [stylish]', () => {
     expect(genDiff(before, after, 'stylish')).toBe(output.stylish);
