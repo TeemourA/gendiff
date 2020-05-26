f8ff1e69d8d3299515e823dad4424e44cf3181fc
TeemourA <taxprogramming@gmail.com>
2020-05-26 23:27:59 +0300

    add new prop for nodes: depth

--- src/diff.js
+++ src/diff.js
@@ -1,30 +1,40 @@
 import _ from 'lodash';
 
+const initialDepth = 1;
+
 const getUniqKeys = (obj1, obj2) => _.union(Object.keys(obj1), Object.keys(obj2));
 
-const buildDiff = (obj1, obj2) => {
+const buildDiff = (obj1, obj2, depth = initialDepth) => {
   const keys = getUniqKeys(obj1, obj2);
 
   const diff = keys.map((key) => {
     if (!_.has(obj1, key)) {
-      return { key, type: 'added', value: obj2[key] };
+      return {
+        key, type: 'added', value: obj2[key], depth,
+      };
     }
 
     if (!_.has(obj2, key)) {
-      return { key, type: 'deleted', value: obj1[key] };
-    }
-
-    if (obj1[key] === obj2[key]) {
-      return { key, type: 'unchanged', value: obj1[key] };
+      return {
+        key, type: 'deleted', value: obj1[key], depth,
+      };
     }
 
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      const children = buildDiff(obj1[key], obj2[key]);
-      return { key, type: 'nested', children };
+      const children = buildDiff(obj1[key], obj2[key], depth + 1);
+      return {
+        key, type: 'nested', children, depth,
+      };
+    }
+
+    if (obj1[key] !== obj2[key]) {
+      return {
+        key, type: 'changed', oldVal: obj1[key], newVal: obj2[key], depth,
+      };
     }
 
     return {
-      key, type: 'changed', oldVal: obj1[key], newVal: obj2[key],
+      key, type: 'unchanged', value: obj1[key], depth,
     };
   });
 
