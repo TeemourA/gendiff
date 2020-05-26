294a792980ce8fcc00666f07bcf5fca93047c267
TeemourA <taxprogramming@gmail.com>
2020-05-26 23:25:40 +0300

    refactor

--- src/parsers.js
+++ src/parsers.js
@@ -1,32 +1,39 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 import _ from 'lodash';
+import path from 'path';
 
-const iniParse = (data) => {
-  const normalizeValue = (value) => {
-    const isNumber = (typeof value === 'string') && Number(value);
-    return isNumber ? Number(value) : undefined;
-  };
+const normalizeValue = (value) => {
+  const isNumber = (typeof value === 'string') && Number(value);
+  return isNumber ? Number(value) : undefined;
+};
 
+const parseIni = (data) => {
   const object = ini.parse(data);
   const normalizedObject = _.cloneDeepWith(object, normalizeValue);
   return normalizedObject;
 };
 
+const normalizeFormat = (filename) => {
+  const format = path.extname(filename).slice(1);
+
+  return format === 'yml' ? 'yaml' : format;
+};
+
 const parsers = {
   json: JSON.parse,
   yaml: yaml.safeLoad,
-  ini: iniParse,
+  ini: parseIni,
 };
 
-const isNotSupported = (ext) => parsers[ext] === undefined;
+export default (data, format) => {
+  const normalizedFormat = normalizeFormat(format);
 
-export default (data, ext) => {
-  if (isNotSupported(ext)) {
+  if (!parsers[normalizedFormat]) {
     throw new Error('Unsupported format');
   }
 
-  const parse = parsers[ext];
+  const parse = parsers[normalizedFormat];
 
   return parse(data);
 };
